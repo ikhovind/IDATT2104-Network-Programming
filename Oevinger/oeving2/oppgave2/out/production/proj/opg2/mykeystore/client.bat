2154cbf1b19540cfa6a669e717b644841d126092
Ingebrigt Hovind <ing.hovind@hotmail.com>
2021-01-27 16:01:48 +0100

    folders

--- Oevinger/oeving2/oppgave2/out/production/proj/opg2/mykeystore/client.bat
+++
@@ -1,4 +0,0 @@
-java -jar -Djavax.net.ssl.trustStore="C:\Users\Vaddy\Desktop\New folder (2)\Nettprog\proj\src\opg2\mykeystore\examplestore" -Djavax.net.ssl.trustStorePassword=password "C:\Users\Vaddy\Desktop\New folder (2)\Nettprog\proj\src\resources\client-jar\JavaSSLClient.jar"
-
-
-java -jar -Djavax.net.ssl.trustStore=keystore -Djavax.net.ssl.trustStorePassword=password "...JavaSSLClient.jar"
