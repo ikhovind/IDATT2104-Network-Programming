3269b5bddfb1058433dd6f7e7174e4591ccc5710
Ingebrigt <ing.hovind@hotmail.com>
2021-02-01 15:17:40 +0100

    gotten further with ssl

---
+++ Oevinger/oeving2/src/opg2/ingebrigt/JavaSSLClient.java
@@ -0,0 +1,54 @@
+package opg2.ingebrigt;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.SSLSocketFactory;
+
+/**
+ * @web http://java-buddy.blogspot.com/
+ */
+public class JavaSSLClient {
+
+    static final int port = 8000;
+
+    public static void main(String[] args) {
+        System.setProperty("javax.net.ssl.keyStore",
+                "/home/ingebrigt/Documents/uni-2/nettverksprog/Oevinger/oeving2/src/opg2/ingebrigt/mykeystore/examplestore");
+        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
+
+        SSLSocketFactory sslSocketFactory =
+                (SSLSocketFactory)SSLSocketFactory.getDefault();
+        try {
+            Socket socket = sslSocketFactory.createSocket("localhost", port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            try (BufferedReader bufferedReader =
+                         new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                Scanner scanner = new Scanner(System.in);
+                while(true){
+                    System.out.println("Enter something:");
+                    String inputLine = scanner.nextLine();
+                    if(inputLine.equals("q")){
+                        break;
+                    }
+
+                    out.println(inputLine);
+                    System.out.println(bufferedReader.readLine());
+                }
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(JavaSSLClient.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
