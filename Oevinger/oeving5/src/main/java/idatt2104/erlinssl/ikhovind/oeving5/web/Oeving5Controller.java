a3a4a26649d5de7d0cd694fd5ebcc898dc925266
Ingebrigt <ing.hovind@hotmail.com>
2021-03-02 15:40:23 +0100

    now writes to file

--- Oevinger/oeving5/src/main/java/idatt2104/erlinssl/ikhovind/oeving5/web/Oeving5Controller.java
+++ Oevinger/oeving5/src/main/java/idatt2104/erlinssl/ikhovind/oeving5/web/Oeving5Controller.java
@@ -5,11 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.spring5.expression.Mvc;
 
+import java.io.BufferedWriter;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.text.StringEscapeUtils;
+
 @Controller
 public class Oeving5Controller {
 
     @RequestMapping("/welcome")
     public String welcome() {
+        return "index";
+    }
+
+    @PostMapping("/programs")
+    public String enterCode(@RequestBody String input) throws IOException {
+        String unEscapedHTML = URLDecoder.decode(input);
+        BufferedWriter writer = new BufferedWriter(new FileWriter("out.cpp"));
+
+        System.out.println(unEscapedHTML);
+        writer.write(unEscapedHTML.substring(10));
+        writer.close();
         return "index";
     }
 }
