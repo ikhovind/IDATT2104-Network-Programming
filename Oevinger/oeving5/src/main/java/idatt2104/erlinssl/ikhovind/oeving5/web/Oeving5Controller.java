9153543e281f411e79273102cf316c50867c8336
Ingebrigt <ing.hovind@hotmail.com>
2021-03-03 14:26:03 +0100

    now works with percentage sign

--- Oevinger/oeving5/src/main/java/idatt2104/erlinssl/ikhovind/oeving5/web/Oeving5Controller.java
+++ Oevinger/oeving5/src/main/java/idatt2104/erlinssl/ikhovind/oeving5/web/Oeving5Controller.java
@@ -14,13 +14,13 @@ import java.nio.charset.StandardCharsets;
 @Controller
 public class Oeving5Controller {
 
-    @RequestMapping("/welcome")
+    @RequestMapping("/")
     public String welcome() {
-        return "index.html";
+        return "index";
     }
 
     @PostMapping("/programs")
-    public ResponseEntity enterCode(Model model, @RequestBody String input) throws IOException, InterruptedException {
+    public ResponseEntity<String> enterCode(@RequestBody String input) throws IOException, InterruptedException {
         String unEscapedHTML = URLDecoder.decode(input, StandardCharsets.UTF_8);
         if(unEscapedHTML.contains("inputCode=")) unEscapedHTML = unEscapedHTML.substring("inputCode=".length());
         BufferedWriter writer = new BufferedWriter(new FileWriter("docker/out.cpp"));
@@ -34,20 +34,17 @@ public class Oeving5Controller {
             Process runImage = Runtime.getRuntime().exec("docker run temp");
             runImage.waitFor();
             String codeOutput = new String(runImage.getInputStream().readAllBytes());
-            System.out.println(codeOutput);
-            model.addAttribute("name", codeOutput);
+
             Runtime.getRuntime().exec("docker image rm temp");
-            
-            return new ResponseEntity(codeOutput, HttpStatus.OK);
+
+            return new ResponseEntity<String>(codeOutput, HttpStatus.OK);
         }
         //exitcode other than 0 means an error (code didn't compile successfully probably)
         else{
             //TODO return an error message to output
-            model.addAttribute("name", "you fool you absolute bufoon");
 
-            byte[] out = new FileInputStream("src/main/resources/templates/index.html").readAllBytes();
             //todo figure out what error to use here
-            return new ResponseEntity(out, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity("compilation error", HttpStatus.BAD_REQUEST);
 
         }
     }
