1a71553718edd78424a86cd5daf0587d8d06caad
Ingebrigt <ing.hovind@hotmail.com>
2021-03-08 18:45:41 +0100

    hashes string and responds, but client claims invalid http response

--- Oevinger/oeving6/src/oeving6server.js
+++ Oevinger/oeving6/src/oeving6server.js
@@ -1,5 +1,5 @@
 
-
+var crypto = require('crypto')
 const net = require('net');
 
 // Simple HTTP server responds with a simple WebSocket client test
@@ -11,13 +11,11 @@ const httpServer = net.createServer((connection) => {
     <meta charset="UTF-8" />
   </head>
   <body>
-  <input id="inputButton" type="submit" value="Submit">
-  <br>
     WebSocket test page
     <script>
       let ws = new WebSocket('ws://localhost:3001');
       ws.onmessage = event => alert('Message from server: ' + event.data);
-      ws.onopen = () => ws.send('hello'); 
+      ws.onopen = () => ws.send('hello');
       
     </script>
   </body>
@@ -26,7 +24,6 @@ const httpServer = net.createServer((connection) => {
         connection.write('HTTP/1.1 200 OK\r\nContent-Length: ' + content.length + '\r\n\r\n' + content);
     });
 });
-
 httpServer.listen(3000, () => {
     console.log('HTTP server listening on port 3000');
 });
@@ -37,18 +34,39 @@ const wsServer = net.createServer((connection) => {
 
     connection.on('data', (data) => {
         console.log('Data received from client: ', data.toString());
+        //todo nothing is more permanent than a temporary solution, this is very scuffed
+        let key = (data.toString().substr((data.toString()).indexOf("Sec-WebSocket-Key") + "Sec-WebSocket-Key".length+2, "VhkJDv+ei89JaGJpX6i4AQ==".length));
+        //string defined my rfc6455
+        key.concat("258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+        console.log(key);
+        let shasum = crypto.createHash('sha1');
+        shasum.update(key);
+        let hash = shasum.digest('hex');
+        console.log(hash + '\n');
+        let httpResponse = ('HTTP/1.1 101 Switching Protocols\r\n' +
+        'Upgrade: websocket\r\n' +
+        'Connection: Upgrade\r\n' +
+        'Sec-WebSocket-Accept: ' + hash +'\n');
+
+        console.log('response written to client:\n' + httpResponse);
+        connection.write(httpResponse);
+
     });
+
+
+    connection.write("hello");
 
     connection.on('end', () => {
+
         console.log('Client disconnected');
     });
-    connection.write("hei hva skjer");
-
 });
 wsServer.on('error', (error) => {
+
     console.error('Error: ', error);
 });
 wsServer.listen(3001, () => {
+
     console.log('WebSocket server listening on port 3001');
 });
 
