89d79aa00997809516132cf97b761cd38bd4ffd8
Ingebrigt <ing.hovind@hotmail.com>
2021-03-09 14:17:57 +0100

    forsøkt å koke men får bare feil

--- Oevinger/oeving6/src/oeving6server.js
+++ Oevinger/oeving6/src/oeving6server.js
@@ -1,6 +1,8 @@
 
 var crypto = require('crypto')
 const net = require('net');
+const { createHash } = require('crypto');
+
 
 // Simple HTTP server responds with a simple WebSocket client test
 const httpServer = net.createServer((connection) => {
@@ -36,20 +38,16 @@ const wsServer = net.createServer((connection) => {
         console.log('Data received from client: ', data.toString());
         //todo nothing is more permanent than a temporary solution, this is very scuffed
         let key = (data.toString().substr((data.toString()).indexOf("Sec-WebSocket-Key") + "Sec-WebSocket-Key".length+2, "VhkJDv+ei89JaGJpX6i4AQ==".length));
-        //string defined my rfc6455
-        key.concat("258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
-        console.log(key);
-        let shasum = crypto.createHash('sha1');
-        shasum.update(key);
-        let hash = shasum.digest('hex');
-        console.log(hash + '\n');
-        let httpResponse = ('HTTP/1.1 101 Switching Protocols\r\n' +
-        'Upgrade: websocket\r\n' +
-        'Connection: Upgrade\r\n' +
-        'Sec-WebSocket-Accept: ' + hash +'\n');
 
-        console.log('response written to client:\n' + httpResponse);
-        connection.write(httpResponse);
+        const hash = createHash('sha1')
+            .update(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
+            .digest('base64');
+
+        const responseHeaders = [ 'HTTP/1.1 101 Web Socket Protocol Handshake', 'Upgrade: WebSocket', 'Connection: Upgrade', `Sec-WebSocket-Accept: ${hash}` , 'Sec-WebSocket-Protocol: json'];
+        let response = responseHeaders.join('\r\n') + '\r\n\r\n';
+        console.log(response);
+        connection.write(response);
+        connection.pipe(connection);
 
     });
 
@@ -70,3 +68,9 @@ wsServer.listen(3001, () => {
     console.log('WebSocket server listening on port 3001');
 });
 
+function generateAcceptValue (acceptKey) {
+    return crypto
+        .createHash('sha1')
+        .update(acceptKey + '258EAFA5-E914–47DA-95CA-C5AB0DC85B11', 'binary')
+        .digest('base64');
+}
