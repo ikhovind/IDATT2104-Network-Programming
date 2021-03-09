74abab39fca2c409c6cf23f3a7e5461c96f0639e
Ingebrigt <ing.hovind@hotmail.com>
2021-03-09 18:55:23 +0100

    now recieves message from client successfully

--- Oevinger/oeving6/src/oeving6server.js
+++ Oevinger/oeving6/src/oeving6server.js
@@ -2,75 +2,84 @@
 var crypto = require('crypto')
 const net = require('net');
 const { createHash } = require('crypto');
-
+const http = require('http');
 
 // Simple HTTP server responds with a simple WebSocket client test
-const httpServer = net.createServer((connection) => {
-    connection.on('data', () => {
-        let content = `<!DOCTYPE html>
+const requestListener = function (req, res) {
+
+    res.setHeader("Content-Type", "text/html");
+    let content = `<!DOCTYPE html>
 <html>
   <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8" />    
   </head>
   <body>
     WebSocket test page
     <script>
-      let ws = new WebSocket('ws://localhost:3001');
+      let ws = new WebSocket('ws://localhost:8080');
       ws.onmessage = event => alert('Message from server: ' + event.data);
-      ws.onopen = () => ws.send('hello');
+      ws.onopen = () => ws.send("hei");
       
     </script>
   </body>
 </html>
 `;
-        connection.write('HTTP/1.1 200 OK\r\nContent-Length: ' + content.length + '\r\n\r\n' + content);
-    });
-});
-httpServer.listen(3000, () => {
-    console.log('HTTP server listening on port 3000');
-});
 
-// Incomplete WebSocket server
-const wsServer = net.createServer((connection) => {
-    console.log('Client connected');
+        res.writeHead(200);
+        res.end(content);
+};
+const server = http.createServer(requestListener);
+server.listen(8080);
 
-    connection.on('data', (data) => {
-        console.log('Data received from client: ', data.toString());
-        //todo nothing is more permanent than a temporary solution, this is very scuffed
-        let key = (data.toString().substr((data.toString()).indexOf("Sec-WebSocket-Key") + "Sec-WebSocket-Key".length+2, "VhkJDv+ei89JaGJpX6i4AQ==".length));
-
-        const hash = createHash('sha1')
-            .update(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
-            .digest('base64');
-
-        const responseHeaders = [ 'HTTP/1.1 101 Web Socket Protocol Handshake', 'Upgrade: WebSocket', 'Connection: Upgrade', `Sec-WebSocket-Accept: ${hash}` , 'Sec-WebSocket-Protocol: json'];
-        let response = responseHeaders.join('\r\n') + '\r\n\r\n';
-        console.log(response);
-        connection.write(response);
-        connection.pipe(connection);
-
-    });
-
-
-    connection.write("hello");
-
-    connection.on('end', () => {
-
-        console.log('Client disconnected');
-    });
-});
-wsServer.on('error', (error) => {
-
-    console.error('Error: ', error);
-});
-wsServer.listen(3001, () => {
-
-    console.log('WebSocket server listening on port 3001');
-});
-
-function generateAcceptValue (acceptKey) {
-    return crypto
-        .createHash('sha1')
-        .update(acceptKey + '258EAFA5-E914–47DA-95CA-C5AB0DC85B11', 'binary')
+server.on('upgrade', (req, socket) => {
+    // Read the websocket key provided by the client:
+    const acceptKey = req.headers['sec-websocket-key'];
+    // Generate the response value to use in the response:
+    const hash = createHash('sha1')
+        .update(acceptKey + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
         .digest('base64');
-}
+    // Write the HTTP response into an array of response lines:
+    const responseHeaders = [ 'HTTP/1.1 101 Web Socket Protocol Handshake', 'Upgrade: WebSocket', 'Connection: Upgrade', `Sec-WebSocket-Accept: ${hash}` ];
+
+
+    // Read the subprotocol from the client request headers:
+    const protocol = req.headers['sec-websocket-protocol'];
+// If provided, they'll be formatted as a comma-delimited string of protocol
+// names that the client supports; we'll need to parse the header value, if
+// provided, and see what options the client is offering:
+    const protocols = !protocol ? [] : protocol.split(',').map(s => s.trim());
+// To keep it simple, we'll just see if JSON was an option, and if so, include
+// it in the HTTP response:
+    if (protocols.includes('json')) {
+        // Tell the client that we agree to communicate with JSON data
+        responseHeaders.push(`Sec-WebSocket-Protocol: json`);
+    }
+    // Write the response back to the client socket, being sure to append two
+    // additional newlines so that the browser recognises the end of the response
+    // header and doesn't continue to wait for more header data:
+    console.log(responseHeaders.join('\r\n') + '\r\n\r\n');
+    socket.write(responseHeaders.join('\r\n') + '\r\n\r\n');
+
+    //is run when client calls ws.send
+    socket.on("data", (req, socket) => {
+        console.log(req);
+        let response = [];
+        if(req[0] == parseInt("81", 16)){
+            console.log("text message");
+            //masked if first bit is 1
+            let masked = (req[1] & 128) === 128
+            //length in bytes given by next seven bits
+            let length = (req[1] & 127);
+            console.log(masked);
+
+            console.log(length);
+
+            let dataStart = 6;
+            for(let i = dataStart; i < dataStart + length; i++){
+                let byte = req[i] ^ req[2 + ((i - dataStart) % 4)];
+                response[i - dataStart] = (String.fromCharCode(byte));
+            }
+            response.forEach(s=> console.log(s))
+        }
+    });
+});
